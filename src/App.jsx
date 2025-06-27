2c9fe4ab744a8c239e88d4d5dd5a0f6487282e6c
masab2005 <muhammad.masab05@gmail.com>
2025-06-28 00:08:49 +0500

    Fix build errors and remove Player.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import Game from './game/Game'
-import GameWrapper from './game/GameWrapper'
-import Player from './game/player'
 import LoggedIN from './account/LoggedIn'
-import LogOutBtn from './account/LogOutBtn'
-import Leaderboard from './navBar/Leaderboard'
 
 function App() {
   return (
